9a6f913e9c499941e941ef1e9a2deeb8a356d8ca
Shelby2 <shelbsjb@hotmail.com>
2019-04-06 13:58:18 -0600

    working app

---
+++ code/clickygame/src/components/card/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} />
+      </div>
+      <div className="content">
+        
+            <strong>Name:</strong> {props.name}
+          
+          </div>  
+    </div>
+  );
+}
+
+export default Card;
